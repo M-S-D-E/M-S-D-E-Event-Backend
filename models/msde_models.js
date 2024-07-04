5c0c254291e1bc775862372cb77539c0072595b4
Melisa <melamaachia@gmail.com>
2024-07-04 16:17:57 +0000

    true

--- models/msde_models.js
+++ models/msde_models.js
@@ -2,16 +2,16 @@ import { Schema, model } from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
 
 const eventSchema = new Schema({
-    eventNames: { type: String },
-    eventDate: { type: Date },
-    eventType: { type:String, enum: ['concerts', 'serminar', 'tasting'] },
-    price: { type: Number },
-    eventCompany: { type: String },
-    location: { type: String },
-    eventStatus: { type: String, enum: ['upcoming', 'completed', 'postponed'] },
-    createAt: { type: Date },
-    updatedAt: { type: Date },
+    eventNames: { type: String, required: true },
+    eventDate: { type: Date, required: true },
+    eventType: { type:String, enum: ['concerts', 'serminar', 'tasting'], required: true },
+    price: { type: Number , required: true},
+    eventCompany: { type: String , required: true},
+    location: { type: String , required: true},
+    eventStatus: { type: String, enum: ['upcoming', 'completed', 'postponed'], required: true },
     image: { type: String, required: true }
+},{
+    timestamps:true
 })
 
 eventSchema.plugin(toJSON);
